48da285c4b9362a45b7df21cb2e41dec61fceab3
Amiran <protonguy9@gmail.com>
2022-11-04 18:03:55 +0400

    Balance

--- src/main/java/net/amik/coursemod/item/custom/ModTiers.java
+++ src/main/java/net/amik/coursemod/item/custom/ModTiers.java
@@ -6,7 +6,7 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.common.ForgeTier;
 
 public class ModTiers {
-    public static final ForgeTier TUNGSTEN = new ForgeTier(1,1000,1f,3f,
+    public static final ForgeTier TUNGSTEN = new ForgeTier(1,1000,1f,3.5f,
     10, BlockTags.NEEDS_IRON_TOOL, () -> Ingredient.of(ModItems.TUNGSTEN_INGOT.get()));
     //Ingredient.of = Чем чинить
 }
